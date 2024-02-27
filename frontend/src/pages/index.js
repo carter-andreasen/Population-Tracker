3fe3e450aa25aa4dcd92a480bedc697e9403dc7b
Carter Andreasen <carterandreasen@infra04-wg011.eduroam.wireless.umn.edu>
2024-02-26 21:42:11 -0600

    Added a paragraph element and tried to add a list element

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -6,13 +6,19 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-left justify-between p-24 ${inter.className}`}
     >
-          <h1 className={`m-0 text-9xl max-w-[30ch] text-sm opacity-100 text-balance`}>
-            Title
-          </h1>
-        
-      
+      <h1 className={`m-0 text-9xl max-w-[30ch] text-lg opacity-100 text-balance`}>
+      Title
+      </h1>
+      <p className={'flex min-h-screen text-sm opacity-90' }>
+      This is a related paragraph of text!
+      </p>
+      <ol type="1">
+        <li>First</li>
+        <li>Second</li>
+        <li>Third</li>
+      </ol>
     </main>
   );
 }
